9e7dd72eacaa32bf8e1a822007e824f3890a8a65
Eren <erenbutun3598@gmail.com>
2022-10-13 20:11:20 +0300

    section 3

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,16 +14,18 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        private readonly ShopContext _context;
-        public ProductController(ShopContext context)
+        
+        private readonly IProductRepository _repo;
+        public ProductController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
+           
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts(){
             
-            var products = await _context.Set<Product>().ToListAsync();
+            var products = await _repo.GetProductsAsync();
 
             return Ok(products);
         }
@@ -30,7 +33,23 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProducts(int id){
             
-          return await _context.Set<Product>().FindAsync(id);
+          return await _repo.GetProductByIdAsync(id);
         }
+
+        [HttpGet("brands")]
+
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+
+        
+        [HttpGet("types")]
+
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
+        
     }
 }
--- Infrastructure/Data/ShopContext.cs
+++ Infrastructure/Data/ShopContext.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -13,7 +14,16 @@ namespace Infrastructure.Data
         {
         }
 
-        DbSet<Product>Products {get; set;}
+        public DbSet<Product>Products {get; set;}
+        public DbSet<ProductBrand> ProductBrands{ get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            base.OnModelCreating(model);
+            model.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
         
     }
 }
